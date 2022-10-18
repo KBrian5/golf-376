af3b74d0d64d49260d78d5baef1c5063b1dc2084
Kevin Salgado <111696484+KBrian5@users.noreply.github.com>
2022-10-18 14:28:11 -0500

    Add files via upload

--- scripts/data.js
+++ scripts/data.js
@@ -1,5 +1,30 @@
 const golfGames = [
   {
+    game: "Solid Octo Happiness",
+    author: "Kevin Salgado",
+    url: "https://kbrian5.github.io/solid-octo-happiness/",
+  },
+  {
+    game: "something2",
+    author: "someone2",
+    url: "somewhere2",
+  },
+  {
+    game: "something3",
+    author: "someone3",
+    url: "somewhere4",
+  },
+  {
+    game: "something3",
+    author: "someone3",
+    url: "somewhere4",
+  },
+  {
+    game: "something3",
+    author: "someone3",
+    url: "somewhere4",
+  },
+  {
     game: "something",
     author: "someone",
     url: "somewhere",
@@ -8,6 +33,66 @@ const golfGames = [
     game: "something2",
     author: "someone2",
     url: "somewhere2",
+  },
+  {
+    game: "something3",
+    author: "someone3",
+    url: "somewhere4",
+  },
+  {
+    game: "something3",
+    author: "someone3",
+    url: "somewhere4",
+  },
+  {
+    game: "something3",
+    author: "someone3",
+    url: "somewhere4",
+  },
+  {
+    game: "something",
+    author: "someone",
+    url: "somewhere",
+  },
+  {
+    game: "something2",
+    author: "someone2",
+    url: "somewhere2",
+  },
+  {
+    game: "something3",
+    author: "someone3",
+    url: "somewhere4",
+  },
+  {
+    game: "something3",
+    author: "someone3",
+    url: "somewhere4",
+  },
+  {
+    game: "something3",
+    author: "someone3",
+    url: "somewhere4",
+  },
+  {
+    game: "something",
+    author: "someone",
+    url: "somewhere",
+  },
+  {
+    game: "something2",
+    author: "someone2",
+    url: "somewhere2",
+  },
+  {
+    game: "something3",
+    author: "someone3",
+    url: "somewhere4",
+  },
+  {
+    game: "something3",
+    author: "someone3",
+    url: "somewhere4",
   },
   {
     game: "something3",
--- scripts/golf-script.js
+++ scripts/golf-script.js
@@ -3,12 +3,12 @@ $(function () {
 
   $.each(golfGames, (index, data) => {
     $("#gameRow").append(`
-    <div class="col-sm-5 mx-auto mt-5">
+    <div class="col-sm-3 mx-auto mt-5">
         <div class="card">
           <h2 class="card-title"> ${golfGames[index].game} </h2>
           <div class="card-body">${golfGames[index].author}</div>
           <div class="card-footer">
-            <button class="btn btn-primary">game link</button>
+            <a href="${golfGames[index].url}" target="_blank" class="btn btn-primary">game link</a>
           </div>
         </div>
       </div>
